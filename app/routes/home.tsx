ba4a9160325146d4d36e7519f1a4c260f9727441
Tyler Nguyen <tainh.work@gmail.com>
2025-06-06 14:24:03 +0700

    init commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,19 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Button } from "~/components/ui/button"
+
+import type { Route } from "./+types/home"
+import { LoginForm } from "~/components/login-form"
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
-  ];
+  ]
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <LoginForm/>
+    </div>
+  )
 }
